980adaba746fb8c4e1e625bd8fae7a2a3618dcca
darrionn33 <darrionn33@gmail.com>
2023-11-03 19:20:07 +0530

    fixed a bug
    removed the backslash escape from the script

--- create-webpack-app.sh
+++ create-webpack-app.sh
@@ -3,11 +3,11 @@ read -p "Paste repo name: " reponame
 git clone git@github.com:darrionn33/$reponame.git
 cd $reponame
 npm init -y
-npm install ---save-dev webpack webpack-cli html-webpack-plugin webpack-dev-server webpack-merge style-loader css-loader copy-webpack-plugin put-selector
+npm install ---save-dev webpack webpack-cli html-webpack-plugin webpack-dev-server webpack-merge style-loader css-loader copy-webpack-plugin
 cp -r ~/Templates/Web\ Dev/Webpack/. .
 npm pkg set 'scripts.build'='webpack --config webpack.prod.js'
 npm pkg set 'scripts.start'='webpack serve --open --config webpack.dev.js'
-npm pkg set 'scripts.gh-pages'='npm run build && git add . && git commit -m \"updating gh-pages site.\" && git push && git subtree push --prefix dist origin gh-pages'
+npm pkg set 'scripts.gh-pages'='npm run build && git add . && git commit -m "updating gh-pages site." && git push && git subtree push --prefix dist origin gh-pages'
 npm pkg set 'scripts.gh-pages-only'='git subtree push --prefix dist origin gh-pages'
 npm pkg set 'name'=$reponame
 touch README.md
